ab5ec9bfcd033371ca5e25d0eda2c70e9f40cfc1
Jeremy Nelson <jermnelson.gmail.com>
2020-09-06 14:19:11 -0600

    Updated library app

--- bibliotheca/library.py
+++ bibliotheca/library.py
@@ -4,6 +4,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route("/dragon-cataloger")
+def dragon_cataloger():
+    return render_template("dragon-cataloger.html")
+
+@app.route('/head-librarian')
+def head_librarian():
+    return render_template("head-librarian.html")
+
+@app.route("/shelving-clerk.html")
+def shelving_clerk():
+    return render_template("shelving-clerk.thml")
+
+@app.route("/sys-admin")
+def system_admin():
+    return render_template("sys-admin.html")
+
 @app.route("/")
 def home():
     return render_template("index.html")
